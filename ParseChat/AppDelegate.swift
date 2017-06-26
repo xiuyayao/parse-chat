bc11867d32bce9274d4458d837a86d5acfe2f9e6
Xiuya Yao <xiuyayao@xiuyayao.dhcp.thefacebook.com>
2017-06-26 11:38:42 -0700

    More initial commits

--- ParseChat/AppDelegate.swift
+++ ParseChat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration) in
+            configuration.applicationId = "parseChatID"
+            configuration.server = "https://blooming-thicket-89345.herokuapp.com/parse"
+        }))
         return true
     }
 
